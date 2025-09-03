c5a2ca8baaf19312f294f0fa8ee92cf6b2f3a4f8
Somayeh <somayeh.khanbabaei21@gmail.com>
2025-09-03 15:11:14 +0200

    use real data in input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,22 +1,30 @@
-// src/components/Input.jsx
-export default function Input({ type = "text", placeholder, icon: Icon }) {
-    return (
-      <div
-        className="flex items-center gap-2 w-full h-11 px-3 rounded-lg
+export default function Input({
+  type = "text",
+  placeholder,
+  icon: Icon,
+  value,
+  onChange,
+  ...props
+}) {
+  return (
+    <div
+      className="flex items-center gap-2 w-full h-11 px-3 rounded-lg
                    bg-white/10 border border-white/20
                    text-white placeholder-white/70
                    focus-within:border-indigo-400 transition"
-      >
-        {/* Icon */}
-        {Icon && <Icon className="w-5 h-5 text-white/70" />}
-  
-        {/* Input field */}
-        <input
-          type={type}
-          placeholder={placeholder}
-          className="flex-1 bg-transparent outline-none placeholder-white/70"
-        />
-      </div>
-    );
-  }
-  
+    >
+      {/* Icon */}
+      {Icon && <Icon className="w-5 h-5 text-white/70" />}
+
+      {/* Input field */}
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className="flex-1 bg-transparent outline-none placeholder-white/70"
+        {...props}
+      />
+    </div>
+  );
+}
