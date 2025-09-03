8c7982b84e995e9934b30b7aea5dbc9eba4d4ce7
Somayeh <somayeh.khanbabaei21@gmail.com>
2025-09-03 15:11:49 +0200

    update links and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-
 import { Navigate, Route, Routes } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
 import ProjectDetails from "./pages/ProjectDetails";
+import Tasks from "./pages/Tasks";
+import Calendar from "./pages/Calendar";
+import Files from "./pages/Files";
+import Settings from "./pages/Settings";
+import { AuthProvider } from "./contexts/AuthContext";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to='login' replace />} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="/signup" element={<SignUp/>}/>
-      <Route path="/dashboard" element={<Dashboard/>} />
-      <Route path="/profile" element={<Profile/>} />
-      <Route path="/projects/:id" element={<ProjectDetails/>} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Navigate to="login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/projects/:id" element={<ProjectDetails />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/files" element={<Files />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </AuthProvider>
   );
 }
